d1c247f4a07ee01a84a7acd8992585ea26733248
Eugene McCormack <c17745919@mytudublin.ie>
2021-04-21 22:50:25 +0100

    User View Collections Recycler View Partially Working

--- app/src/main/java/com/example/user_collect_my_car/Model/UserModel.kt
+++ app/src/main/java/com/example/user_collect_my_car/Model/UserModel.kt
@@ -2,9 +2,58 @@ package com.example.user_collect_my_car.Model
 
 class UserModel {
 
-    var name:String = ""
-    var phone:String = ""
-    var email:String = ""
-    var image:String = ""
+    var name:String? = null
+    var phone:String? = null
+    var email:String? = null
+    var image:String? = null
+
+    var user: String? = null
+
+    var driver: String? = null
+
+    var driverInfoModel: DriverInfoModel? = null
+
+    var userModel: UserModel? = null
+
+    var origin: String? = null
+
+    var originString: String? = null
+
+    var destination: String? = null
+
+    var destinationString: String? = null
+
+    var durationPickup: String? = null
+
+    var durationDestination: String? = null
+
+    var distancePickup: String? = null
+
+    var distanceDestination: String? = null
+
+    var currentLat = 0.0
+
+    var currentLng = 0.0
+
+    var isDone = false
+
+    var isCancelled = false
+
+    constructor() {
+
+
+    }
+
+    constructor(name: String, phone: String, email: String, image: String, destinationString: String, user: String) {
+        this.name = name
+        this.phone = phone
+        this.email = email
+        this.image = image
+        this.destinationString = destinationString
+        this.user = user
+    }
+
+
+
 
 }
